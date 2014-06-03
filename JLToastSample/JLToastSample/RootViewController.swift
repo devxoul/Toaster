4a2ab02b7b6c3ee9d0697ada4b352db6d78c5c9d
전수열 <devxoul@gmail.com>
2014-06-03 23:24:09 +0900

    Update license.

--- JLToastSample/JLToastSample/RootViewController.swift
+++ JLToastSample/JLToastSample/RootViewController.swift
@@ -1,21 +1,21 @@
 /*
-* RootViewController.m
-*
-*            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-*                    Version 2, February 2013
-*
-* Copyright (C) 2013 Joyfl
-*
-* Everyone is permitted to copy and distribute verbatim or modified
-* copies of this license document, and changing it is allowed as long
-* as the name is changed.
-*
-*            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-*   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-*
-*  0. You just DO WHAT THE FUCK YOU WANT TO.
-*
-*/
+ * RootViewController.swift
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2013-2014 Su Yeol Jeon
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
 
 import UIKit
 

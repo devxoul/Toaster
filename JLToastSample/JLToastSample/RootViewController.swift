8be062dbe0c200c6c7d7009d013a8cea42f980b3
Suyeol Jeon <devxoul@gmail.com>
2014-08-14 12:28:50 +0900

    Add `.show()` to last toast message in sample project.

--- JLToastSample/JLToastSample/RootViewController.swift
+++ JLToastSample/JLToastSample/RootViewController.swift
@@ -33,6 +33,6 @@ class RootViewController: UIViewController {
         JLToast.makeText("Basic JLToast").show()
         JLToast.makeText("You can set duration. `JLToastDelay.ShortDelay` means 2 seconds." +
                          "`JLToastDelay.LongDelay` means 3.5 seconds.", duration: JLToastDelay.LongDelay).show()
-        JLToast.makeText("With delay, JLToast will be shown after delay.", delay: 1, duration: 5)
+        JLToast.makeText("With delay, JLToast will be shown after delay.", delay: 1, duration: 5).show()
     }
 }

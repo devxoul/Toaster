cdeab6cf41f1ace23049660938287d0b12d5f308
Suyeol Jeon <devxoul@gmail.com>
2014-08-15 17:58:13 +0900

    Move `JLToastSample` project to `Sample/JLToastSwiftSample`.

--- Sample/JLToastSwiftSample/JLToastSwiftSample/RootViewController.swift
+++ Sample/JLToastSwiftSample/JLToastSwiftSample/RootViewController.swift
@@ -4,7 +4,7 @@
  *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *                    Version 2, December 2004
  *
- * Copyright (C) 2013-2014 Su Yeol Jeon
+ * Copyright (C) 2013-2014 Suyeol Jeon <devxoul@gmail.com>
  *
  * Everyone is permitted to copy and distribute verbatim or modified
  * copies of this license document, and changing it is allowed as long
@@ -31,7 +31,7 @@ class RootViewController: UIViewController {
 
     func showButtonTouchUpInside() {
         JLToast.makeText("Basic JLToast").show()
-        JLToast.makeText("You can set duration. `JLToastDelay.ShortDelay` means 2 seconds." +
+        JLToast.makeText("You can set duration. `JLToastDelay.ShortDelay` means 2 seconds.\n" +
                          "`JLToastDelay.LongDelay` means 3.5 seconds.", duration: JLToastDelay.LongDelay).show()
         JLToast.makeText("With delay, JLToast will be shown after delay.", delay: 1, duration: 5).show()
     }

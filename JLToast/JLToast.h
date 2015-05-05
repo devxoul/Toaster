53366cdf6033c7132219bf50c6d60a0944c6e035
KimDarren <korean.darren@gmail.com>
2015-05-06 04:32:40 +0900

    Say happy new year to JLToast swift class

--- JLToast/JLToast.h
+++ JLToast/JLToast.h
@@ -2,7 +2,7 @@
  *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *                    Version 2, December 2004
  *
- * Copyright (C) 2013-2014 Suyeol Jeon <devxoul@gmail.com>
+ * Copyright (C) 2013-2015 Suyeol Jeon <devxoul@gmail.com>
  *
  * Everyone is permitted to copy and distribute verbatim or modified
  * copies of this license document, and changing it is allowed as long

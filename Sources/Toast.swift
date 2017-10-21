57ff188b4fc10a93c769e4f24e29d6c7f5cf23aa
Suyeol Jeon <devxoul@gmail.com>
2017-10-21 18:03:42 +0900

    Remove license header from source files

--- Sources/Toast.swift
+++ Sources/Toast.swift
@@ -1,22 +1,3 @@
-/*
- * Toast.swift
- *
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *                    Version 2, December 2004
- *
- * Copyright (C) 2013-2015 Su Yeol Jeon
- *
- * Everyone is permitted to copy and distribute verbatim or modified
- * copies of this license document, and changing it is allowed as long
- * as the name is changed.
- *
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- *  0. You just DO WHAT THE FUCK YOU WANT TO.
- *
- */
-
 import UIKit
 
 public struct Delay {

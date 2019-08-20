306835bb254975ef106667420b2ca2634761b2da
seongho.hong <seongho.hong@navercorp.com>
2019-08-20 13:27:59 +0900

    Fix status bar style issue

--- Sources/ToastWindow.swift
+++ Sources/ToastWindow.swift
@@ -42,7 +42,10 @@ open class ToastWindow: UIWindow {
 
   override open var rootViewController: UIViewController? {
     get {
-      guard !self.isShowing else { return nil }
+      guard !self.isShowing else {
+        self.isShowing = false
+        return nil
+      }
       guard !self.isStatusBarOrientationChanging else { return nil }
       guard let firstWindow = UIApplication.shared.delegate?.window else { return nil }
       return firstWindow is ToastWindow ? nil : firstWindow?.rootViewController

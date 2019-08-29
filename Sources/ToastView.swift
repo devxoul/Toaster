e4d7467259d0ab9975bda4ab9c57f86d24ae53f7
seongho.hong <hsh3592@gmail.com>
2019-08-30 01:07:10 +0900

    Add helper function for setting cornerRadius

--- Sources/ToastView.swift
+++ Sources/ToastView.swift
@@ -221,4 +221,9 @@ open class ToastView: UIView {
     return nil
   }
 
+  /// Helper function for setting cornerRadius to avoid conflicting with UIView extension.
+  @objc public dynamic func cornerRadius(_ cornerRadius: CGFloat) {
+    self.cornerRadius = cornerRadius
+  }
+  
 }

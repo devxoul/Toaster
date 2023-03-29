3f83429aac5f235cdf38163765cd8ade7761cc75
stareta1202 <stareta1202@gmail.com>
2023-03-29 15:51:44 +0900

    Add toast orientation

---
+++ Sources/ToastOrientation.swift
@@ -0,0 +1,13 @@
+//
+//  ToastOrientation.swift
+//  Toaster
+//
+//  Created by Yongjun Lee on 2023/03/29.
+//  Copyright © 2023 Suyeol Jeon. All rights reserved.
+//
+
+import Foundation
+@objc public enum ToastOrientation: Int {
+  case top
+  case bottom
+}
